b63de1ceef0e64e681f37527b1b6f610520ed9db
kelly <getkellygarner@gmail.com>
2023-01-29 20:27:29 +1100

    adjusted na case in bis source

--- R/source_acc_bis_models.R
+++ R/source_acc_bis_models.R
@@ -15,8 +15,8 @@ faux <- FALSE
 # the elements of the two vectors should match up, so position one of each
 # corresponds to the same data and what you want to do with that data
 # this will form the loop that is used to fit the various models
-data_names <- c('acc', 'cacc')
-msv_fnms <- c('acc_mindwin_bis', 'cacc_mindwin_bis') # save names
+data_names <- c('cacc')
+msv_fnms <- c('cacc_mindwin_bis') # save names
 
 # these vectors contain the model files that you want to run, and any appends that
 # should be made to the msv_fnms above (again, make sure that the elements across
@@ -41,7 +41,7 @@ for (i in 1:length(data_names)){
   acc_dat$bis <- scale(acc_dat$bis)
   
   for (j in 1:length(rfs)){
-    if (is.NA(app[j])){
+    if (is.na(app[j])){
       dir_name <- paste(msv_fnms[i], sep="")
     } else {
       dir_name <- paste(msv_fnms[i], app[j], sep="")
