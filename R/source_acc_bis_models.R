17bd713878bf3b89d9c608892e81b0269d884f8d
kelly <getkellygarner@gmail.com>
2023-01-29 17:58:50 +1100

    added most of code to run bis need to do final update once last acc model has run

--- R/source_acc_bis_models.R
+++ R/source_acc_bis_models.R
@@ -10,8 +10,20 @@ new <- TRUE
 verbal <- TRUE
 faux <- FALSE
 
+# these variable define the data that will be called in
+# and how model results will be named when saved
+# the elements of the two vectors should match up, so position one of each
+# corresponds to the same data and what you want to do with that data
+# this will form the loop that is used to fit the various models
 data_names <- c('acc', 'cacc')
-msv_fnms <- c('acc_winplusbis', 'cacc_winplusbis') # save names
+msv_fnms <- c('acc_mindwin_bis', 'cacc_mindwin_bis') # save names
+
+# these vectors contain the model files that you want to run, and any appends that
+# should be made to the msv_fnms above (again, make sure that the elements across
+# the two vectors correspond. these vectors will
+# be looped over within the data/naming loop above
+rfs <- c("acc_mindwin_bis.R")
+app <- c(NA)
 
 for (i in 1:length(data_names)){
   
@@ -19,32 +31,29 @@ for (i in 1:length(data_names)){
   # load data
   ###-----------------------------------------------------
   load(sprintf('../data/derivatives/%s_dat4_model.Rda', data_names[i]))
+  load('../data/derivatives/mind_scores.Rda')
+  mind_sum$sub <- as.factor(mind_sum$sub)
+  acc_dat <- inner_join(acc_dat, mind_sum, by="sub")
+  acc_dat$m <- scale(acc_dat$m)
   load('../data/derivatives/bis_scores.Rda')
   bis_sum$sub <- as.factor(bis_sum$sub)
   acc_dat <- inner_join(acc_dat, bis_sum, by="sub")
   acc_dat$bis <- scale(acc_dat$bis)
   
-  dir_name <- msv_fnms[i]
-  mod_name <- dir_name
-  
-  source('acc_mod-fxbdrgbis_drgbrfx.R')
-  
-  dir_name <- paste(dir_name, 'bbisint', sep="")
-  mod_name <- dir_name
-  source('acc_mod-fxbdrgbbisint_drgbrfx.R')
-
+  for (j in 1:length(rfs)){
+    if (is.NA(app[j])){
+      dir_name <- paste(msv_fnms[i], sep="")
+    } else {
+      dir_name <- paste(msv_fnms[i], app[j], sep="")
+    }
+    mod_name <- dir_name
+    source(rfs[j])
+  }
 }
 
 
-# for accuracy data, allow BIS to interact with drug
-i = 1
-dir_name <- paste(msv_fnms[i], 'bdbisint', sep="")
-mod_name <- dir_name
-source('acc_mod-fxbdrgbbisintdrgbisint_drgbrfx.R')
 
-dir_name <- paste(msv_fnms[i], 'plus3way', sep="")
-mod_name <- dir_name
-source('acc_mod-fxbdrgbdrgbisint_drgbrfx.R')
+
 
 
 
