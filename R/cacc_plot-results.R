d4bcb0c44077f7475384b73d0ea73adc785bff07
kelly <getkellygarner@gmail.com>
2023-01-30 20:26:24 +1100

    plotted results from accuracy models

--- R/cacc_plot-results.R
+++ R/cacc_plot-results.R
@@ -11,36 +11,23 @@ source('fig_label.R')
 ##############################################################
 # WHAT ARE YOU PLOTTING?
 ##############################################################
-#acc <- TRUE # if false, you are plotting contectual acc
-# if (!acc){
-#   # first load accuracy data
-#   load('../data/derivatives/cacc_dat4_model.Rda')
-#   # now load model
-#   load('../data/derivatives/cacc_winplusmind/cacc_winplusmind.Rda')
-#   mod <- mnd
-#   dat_ylim <- c(.45, .75)
-#   dat_yseq <- seq(.45, .75, .05)
-#   dat_ylabs <- c(".45","","","","","",".75")
-#   figinfo = 'cacc'
-#   w <- 12 # in cm
-#   h <- 8 # in cm
-# } else {
-  load('../data/derivatives/acc_dat4_model.Rda')
-  load('../data/derivatives/acc_winplusmind_bmndintdrgmndint/acc_winplusmind_bmndintdrgmndint.Rda')
-  mod <- mndbdrgmnd
-  dat_ylim <- c(.45, .75)
-  dat_yseq <- seq(.45, .75, .05)
-  dat_ylabs <- c(".45","","","","","",".75")
-  figinfo = 'acc'
-  cor_ylim <- c(-0.3, 0.25)
-  cor_yseq <- seq(-0.3, 0.25, .05)
-  cor_ylabs <- c("-0.3","","","","","","","","","","","0.25")
-  cor_xlim <- c(-2.5, 3)
-  cor_xseq <- seq(-2.5, 3, .5)
-  cor_xlabs <- c("-2.5","","","","","0","","","","","","3")
-  w = 17
-  h = 17
-#}
+
+# first load data
+load('../data/derivatives/cacc_dat4_model.Rda')
+# now load model
+load('../data/derivatives/cacc_winplusmind/cacc_winplusmind.Rda')
+mod <- mnd
+dat_ylim <- c(.45, .75)
+dat_yseq <- seq(.45, .75, .05)
+dat_ylabs <- c(".45","","","","","",".75")
+figinfo = 'cacc'
+w <- 12 # in cm
+h <- 12 # in cm
+
+dat_ylim <- c(.45, .75)
+dat_yseq <- seq(.45, .75, .05)
+dat_ylabs <- c(".45","","","","","",".75")
+
 ##############################################################
 # PLOT SETTINGS
 ##############################################################
@@ -83,21 +70,20 @@ sum_dat <- inner_join(acc_dat, est, by="sub")
 # put the data back together, given the winning model for acc
 # to understand the accuracy data I need to:
 # 1. plot block x drug
-# 2. plot the effect of drug, for people scoring high on mindfulness
-# 3. plot the effect of drug for people scoring low on mindfulness
-# 4. plot the parameter posteriors for drug x mindfulness, block, and drug 
-# 5. plot the remaining posterior distributions for supplemental
+# 2. plot the correlation between mindfulness and accuracy
+# 3. plot the parameter posteriors for drug 
+# 4. plot posteriors for block
+# 5. report the remaining posterior distributions in table
 sum_dat <- rbind(sum_dat %>% filter(drug == "levodopa") %>% 
-                     mutate(log_odds = Intercept + b.y*b.x + `b:druglevodopa`*b.x + m.y*m.x + `b:m`*m.x),
-                   sum_dat %>% filter(drug == "placebo") %>%
-                     mutate(log_odds = Intercept + b.y*b.x +  drugplacebo + `b:drugplacebo`*b.x + m.y*m.x + `b:m`*m.x + `drugplacebo:m`*m.x)) %>% 
+                     mutate(log_odds = Intercept + b.y*b.x + `b:druglevodopa`*b.x + m.y*m.x),
+                 sum_dat %>% filter(drug == "placebo") %>%
+                     mutate(log_odds = Intercept + b.y*b.x +  drugplacebo + `b:drugplacebo`*b.x + m.y*m.x)) %>% 
     mutate(p= 1/(1+exp(-log_odds))) %>%
     mutate(obs = tt/td) %>%
     mutate(resid = obs - p)
 
 # quick residuals check
 sum_dat %>% ggplot(aes(x=p, y=resid, colour=sub)) + geom_point() # pretty happy 
-# with that
 
 ###############################################################
 # DATA 4 PLOTS 
@@ -114,23 +100,15 @@ mu_bdrug_pred <- summarySEwithin(data=sum_dat, measurevar="p",
 mu_bdrug_dat$b.x <- as.numeric(mu_bdrug_dat$b.x)
 mu_bdrug_pred$b.x <- as.numeric(mu_bdrug_pred$b.x)
 
-## to show the mindfulness x drug interaction, I will show the difference
-## between drug and placebo, collapsed across block, correlated with mindfulness
-mnddrgi <- sum_dat %>% group_by(sub, b.x) %>%
-                        summarise(diff = obs[drug == "levodopa"] - obs[drug == "placebo"]) %>%
-                        group_by(sub) %>%
-                        summarise(mu_diff = mean(diff))
-mnddat <- sum_dat[,c("sub", "m.x")] %>% distinct()
-mnddrgi <- inner_join(mnddrgi, mnddat, by = "sub")
-
 ###############################################################
 #### set up for full plot
 ###############################################################
 pdf(sprintf("../images/%s_fig.pdf", figinfo),
     width = w/2.54, height = h/2.54) 
 
-plot.mat = matrix(c(1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 5, 5),
+plot.mat = matrix(c(1, 2, 1, 3),
                   nrow = 2, byrow = T)
+
 layout(plot.mat)
 
 par(las=1, mgp=c(2,1,0))
@@ -169,7 +147,7 @@ with(mu_bdrug_dat %>% filter(drug == "levodopa"),
                               angle = 90,
                               length = .025,
                               col=dopa_col))
-legend(x=6.5, y=.55, legend = c("l", "p"),
+legend(x=5.5, y=.55, legend = c("l", "p"),
        col = c(dopa_col,placebo_col), 
        pch = 19, bty = "n", cex = 1)
 # now add lines from the model
@@ -184,26 +162,7 @@ with(mu_bdrug_pred %>% filter(drug == "levodopa"),
 fig_label("A", cex = 2)
 
 ########################################
-# PANEL 2: INTERACTION BETWEEN MINDFULNESS
-# AND EFFECT OF DRUG
-#######################################
-with(mnddrgi, 
-     plot(m.x, mu_diff,
-          type = "p",
-          pch = 19,
-          ylim=cor_ylim,
-          xlim=cor_xlim,
-          col=cor_data_col,
-          bty="n",
-          xlab = "m",
-          ylab = "acc (DA - off)",
-          axes =F))
-axis(side=1, at=cor_xseq, tick=TRUE, labels=cor_xlabs)
-axis(side=2, at=cor_yseq, tick=TRUE, labels=cor_ylabs, las=2) #, labels=c())
-abline(lm(mu_diff ~ m.x, data = mnddrgi), col = "grey17")
-fig_label("B", cex = 2)
-########################################
-# PANELS 4, 5, 6: DENSITY OF THE PARAMETER ESTIMATES FOR BLOCK, DRUG
+# PANELS 2, 3: DENSITY OF THE PARAMETER ESTIMATES FOR BLOCK, DRUG
 # AND DRUG x MIND
 #######################################
 variables(mod)
@@ -212,30 +171,22 @@ variables(mod)
 # posterior_samples is deprecated, use as_draws next time
 fxdrg_draws <- posterior_samples(mod, pars="b_drugplacebo")
 plot(density(fxdrg_draws$b_drugplacebo),
-     col=samples_col,main="", xlab="log odds",
-     ylab="d", bty="n", xlim=c(-0.15, 0.15), axes=F)
-axis(side=1, at = c(-0.15, 0, 0.15), labels=c("-0.15", "0", "0.15"))
-axis(side=2, at=c(0, 25), labels=c("0", "25"), las=2)
+     col=samples_col, main="", xlab="log odds",
+     ylab="d", bty="n", xlim=c(-0.15, 0.25), ylim=c(0,20), axes=F)
+axis(side=1, at = c(-0.15, 0, 0.25), labels=c("-0.15", "0", "0.15"))
+axis(side=2, at=c(0, 20), labels=c("0", "20"), las=2)
 polygon(density(fxdrg_draws$b_drugplacebo), border=samples_col, col=samples_col)
 title("DA vs off")
-fig_label("C", cex = 2)
-
-plot(density(fxdrg_draws$`b_drugplacebo:m`),
-     col=samples_col, main="", xlab="",
-     ylab="", bty="n", xlim=c(-.25, 0.25), axes=F)
-axis(side=1, at = c(-0.25, 0, .25), labels=c("-0.25", "0", "0.25"))
-#axis(side=2, at=c(0, 25), labels=c("0", "25"), las=2)
-polygon(density(fxdrg_draws$`b_drugplacebo:m`), border=samples_col, col=samples_col)
-title("m*drug")
-fig_label("D", cex = 2)
+fig_label("B", cex = 2)
 
 fxb_draws <- posterior_samples(mod, pars="b_b")
 plot(density(fxb_draws$b_b),
      col=samples_col, main="", xlab="",
-     ylab="", bty="n", xlim=c(-0.15, .35), axes=F)
-axis(side=1, at = c(-0.25, 0, .35), labels=c("-0.25", "0", "0.35"))
+     ylab="", bty="n", xlim=c(-.15, 0.3), ylim=c(0,15), axes=F)
+axis(side=1, at = c(-0.15, 0, .3), labels=c("-0.15", "0", "0.3"))
+axis(side=2, at=c(0, 15), labels=c("0", "15"), las=2)
 polygon(density(fxb_draws$b_b), border=samples_col, col=samples_col)
 title("b")
-fig_label("E", cex=2)
+fig_label("C", cex = 2)
 
 dev.off()
--- R/seq_get-observed-and-null-transitions.R
+++ R/seq_get-observed-and-null-transitions.R
@@ -13,14 +13,22 @@ load('../data/derivatives/dat4_seq_model.Rda')
 ### development
 # data from one subject and context 
 
-tmp <- blocked_dat %>% filter(sub == 1 & drug == "placebo" & cond == 1 & b == 1 & t == 2)
+this_trial <- blocked_dat %>% filter(sub == 1 & drug == "placebo" & cond == 1 & b == 1 & t == 2)
 
 
-do_one_trial <- function(...){
-
-  this_trials_doors <- tmp$door
+do_one_trial <- function(this_trial, ndoors=16){
+  # get the permuted null, and return a list for one trial
+  # Kwargs:
+  # -- tmp [data_frame]: data filtered from blocked_dat, for 1 trial
+  # -- ndoors [integer]: number of doors in the display, default 16
+  # Returns:
+  # list(this_trials_seq, null_hyp)
+  
+  this_trials_doors <- this_trial$door
   nset <- ndoors
   # get all the possible ways the trial could have been performed
+  # NOTE: when this_trials_doors >= 15 
+  # Error: cannot allocate vector of size 9742.9 Gb
   all_possible_routes <- do.call(rbind, combinat::permn(this_trials_doors))
   # remove illegal trials if they exist
   dups_idx <- rowSums(t(diff(t(all_possible_routes))) == 0)
@@ -34,8 +42,23 @@ do_one_trial <- function(...){
   # add NA to make this_trials_doors uniform length across iterations
   nttd <- length(this_trials_doors)
   if(nttd < nset) this_trials_doors <- c(this_trials_doors, rep(NA, times = nset-nttd))
-  #### question on how I want to collect the data here - should I make it a 16 x 16 matrix
-  #### with everything in the right place
-  #### same above, for this_trials_doors, but for the vector case
   list(this_trials_doors, this_trials_transitions) 
 } 
+
+this_block <- blocked_dat %>% filter(sub == 1 & sess == 1 & cond == 1 & b == 1)
+summarise_seqs_and_nulls_across_block <- function(this_block){
+  # run through all the trials for one participant, session, context, and block
+  # return the null hypotheses and the actual doors from each trial, summarised
+  # at the block level
+  # Kwargs:
+  # this_block [dataframe] - data from one subject, session, condition and block
+  
+  # Returns:
+  trials <- unique(this_block$t)
+  trials <- trials[trials != 3]
+  lapply(trials, function(x) do_one_trial(this_block %>% filter(t == x)))
+  
+  
+  
+  
+}
