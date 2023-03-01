2295471b6d1b3d762fe63b8d92aefee5dd284a30
kelly <getkellygarner@gmail.com>
2023-03-01 12:19:36 +1100

    added bis to winning mind model for stereo

---
+++ R/source_stereo_bis_model.R
@@ -0,0 +1,49 @@
+# use this code to source model fitting commands stored
+# in separate R files (see below)
+
+rm(list=ls())
+###------------------------------------------------------
+# load packages
+###-----------------------------------------------------
+library(brms)
+library(tidyverse)
+source('verbal.R')
+new <- TRUE
+verbal <- TRUE
+faux <- FALSE
+
+msv_fnms <- c('stereo_winplusbis')
+rfs <- c("stereo_mod-fxbdmndbis_bmindint.R")
+app <- c(NA)
+
+###------------------------------------------------------
+# load data
+###-----------------------------------------------------
+load('../data/derivatives/dat4_seq_model.Rda')
+load('../data/derivatives/mind_scores.Rda')
+load('../data/derivatives/bis_scores.Rda')
+
+sub_var_dat$sub <- as.factor(sub_var_dat$sub)
+mind_sum$sub <- as.factor(mind_sum$sub)
+sub_var_dat <- inner_join(sub_var_dat, mind_sum, by="sub")
+sub_var_dat$drug <- as.factor(sub_var_dat$drug) # makes no difference if fct or chr
+sub_var_dat$m <- scale(sub_var_dat$m)
+bis_sum$sub <- as.factor(bis_sum$sub)
+sub_var_dat <- inner_join(sub_var_dat, bis_sum, by="sub")
+sub_var_dat$bis <- scale(sub_var_dat$bis)
+
+  
+dir_name <- paste(msv_fnms, sep="")
+mod_name <- dir_name
+source(rfs)
+
+
+###------------------------------------------------------
+# perform model comparisons
+###-----------------------------------------------------
+ftmplt = '../data/derivatives/%s/%s.Rda'
+load(file=sprintf(ftmplt, 'stereo_winplusmind_bmindint', 'stereo_winplusmind_bmindint'))
+load(file=sprintf(ftmplt, 'stereo_winplusbis', 'stereo_winplusbis'))
+
+loo_compare(mndbd_bm, mndbdbis_bm)
+# elpd_diff se_diff
---
+++ R/stereo_mod-fxbdmndbis_bmndint_brfx.R
@@ -0,0 +1,35 @@
+## K. Garner, 2022
+## perform modelling of accuracy data as specified in 
+## https://osf.io/2y6pk
+####------------------------------------------------------
+
+###------------------------------------------------------
+# have you run this model before? defined in sourcing file
+###-----------------------------------------------------
+# new <- TRUE
+# verbal <- FALSE
+dir.create(sprintf('../data/derivatives/%s', dir_name), showWarnings=FALSE)
+
+if (new){
+
+  mndbdbis_bm <- brm(formula = log(v) ~ b + drug + m + b:m + bis + (b|sub),
+                    data = sub_var_dat,
+                    warmup = 2000, iter = 10000,
+                    family = skew_normal,
+                    save_pars = save_pars(all=TRUE)) # for model comparisons 
+  mndbdbis_bm <- add_criterion(mndbdbis_bm, "loo",  moment_match=TRUE, reloo=TRUE)
+  
+  # now save!
+  save.image(file = sprintf('../data/derivatives/%s/%s.Rda', dir_name, mod_name))
+  
+} else {
+  
+  load(file = sprintf('../data/derivatives/%s/%s.Rda', dir_name, mod_name))
+}
+
+if (verbal){
+
+  verbal_output(mndbdbis_bm, dir_name = dir_name)
+}
+
+rm(mndbdbis_bm)
