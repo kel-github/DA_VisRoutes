6bf32bb947b40d34d27f71441c65099aecf1f838
kelly <getkellygarner@gmail.com>
2023-03-25 15:56:10 +1100

    up to manip checks

---
+++ R/manipulation_checks.R
@@ -0,0 +1,15 @@
+## written by K.Garner 2023
+## code to analyse manipulation check data from the DA study
+
+## Participant blinding
+## read in the participant blinding data, wrangle into shape and then 
+## plot the data against a binomial distribution
+
+## possible outcomes
+## correct, correct              - p = .25
+## correct, incorrect            - p = .5
+## incorrect, incorrect          - p = .25
+## the theoretical model is p
+
+
+## Mood ratings
