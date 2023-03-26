a57539f57d46b504009ede9a4ff7ec8dacea9f1d
kel-github <getkellygarner@gmail.com>
2023-03-27 10:24:14 +1100

    started working on participant blinding data

--- R/manipulation_checks.R
+++ R/manipulation_checks.R
@@ -1,5 +1,7 @@
 ## written by K.Garner 2023
 ## code to analyse manipulation check data from the DA study
+rm(list=ls())
+library(tidyverse)
 
 ## Participant blinding
 ## read in the participant blinding data, wrangle into shape and then 
@@ -10,6 +12,18 @@
 ## correct, incorrect            - p = .5
 ## incorrect, incorrect          - p = .25
 ## the theoretical model is p
+
+# read in the participant guess data and clean
+gss <- read.csv("../data/derivatives/participant_DA_guess.csv", header=TRUE)
+names(gss) <- c("time", "sub", "ans")
+gss <- gss %>% filter(sub <= 40)
+# code the answers nicely
+gss <- gss %>% mutate(code = if_else(ans == "The placebo (vitamin)", -1, 
+                             if_else(ans == "The active dopamine drug (Madopar)", 1, 0)))
+# remove old participants (excluded), tidy up the remaining participant labels
+# and add the participant sessions
+gss %>% arrange(sub, time)
+
 
 
 ## Mood ratings
